ef7918e8b0bf7eb798d93d5a7ba9d812bfd1be5e
akynos <peter_greenday@web.de>
2017-02-15 18:26:57 +0100

    Added lum_sensor files

---
+++ balloon_main/lum_sensors.cpp
@@ -0,0 +1,37 @@
+#include "lum_sensors.h"
+
+//Instances from balloon_main
+extern SFE_TSL2561 light;
+extern boolean gain;
+extern unsigned int ms;
+
+void initLightSensor() {
+  light.begin();
+  gain = 0;
+  unsigned char time = 3;
+  light.setTiming(gain,time,ms);
+  light.setPowerUp();
+}
+
+float readLightSensor() {
+  unsigned int data0, data1;
+ 
+  if (light.getData(data0,data1))
+  {
+    double lux;    // Resulting lux value
+    boolean good;  // True if neither sensor is saturated
+    good = light.getLux(gain,ms,data0,data1,lux);
+    if (good) {
+      return (float) lux;
+    } else {
+      return -1.f;
+    }
+  }
+  else
+  {
+    byte error = light.getError();
+    //printError(error);
+    return -1.f;
+  }
+  return -1.f;
+}
---
+++ balloon_main/lum_sensors.h
@@ -0,0 +1,9 @@
+#ifndef lum_sensors_h
+#define lum_sensors_h
+#include "basic.h"
+
+
+void initLightSensor();
+float readLightSensor();
+
+#endif
